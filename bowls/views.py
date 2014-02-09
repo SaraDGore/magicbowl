14b3645254820c3bb6b0a003bae19d4b7406a49d
sara <saradgore@gmail.com>
2014-02-08 19:22:38 -0600

    added source display

--- bowls/views.py
+++ bowls/views.py
@@ -11,12 +11,12 @@ def main(request):
     ingredients = Ingredient.objects.filter(listable=True)
     cuisines = Cuisine.objects.all()
     
-    # starting hardcoding in my bowl choice for now while i work on template
-    sauce = Recipe.objects.filter(role='SAUCE').order_by('?')[:1] 
+    # random selection   
     base = Recipe.objects.filter(role='BASE').order_by('?')[:1]
     protein = Recipe.objects.filter(role='PROTEIN').order_by('?')[:1]  
     veggie = Recipe.objects.filter(role='VEGGIE').order_by('?')[:1] 
-    bowl = [sauce[0], base[0], protein[0], veggie[0]]
+    sauce = Recipe.objects.filter(role='SAUCE').order_by('?')[:1] 
+    bowl = [base[0], protein[0], veggie[0], sauce[0]]
 
     data = {"ingredients": ingredients,
             "cuisines": cuisines,
