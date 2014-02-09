6776ad2fc9504dbd38d352d8503f7c072773123f
sara <saradgore@gmail.com>
2014-02-08 19:17:45 -0600

    changed orderby to order_by

--- bowls/views.py
+++ bowls/views.py
@@ -12,19 +12,12 @@ def main(request):
     cuisines = Cuisine.objects.all()
     
     # starting hardcoding in my bowl choice for now while i work on template
-    sauce = Recipe.objects.filter(role='SAUCE').orderby('?')[:1] 
-    base = Recipe.objects.filter(role='BASE').orderby('?')[:1]
-    protein = Recipe.objects.filter(role='PROTEIN').orderby('?')[:1]  
-    veggie = Recipe.objects.filter(role='VEGGIE').orderby('?')[:1] 
+    sauce = Recipe.objects.filter(role='SAUCE').order_by('?')[:1] 
+    base = Recipe.objects.filter(role='BASE').order_by('?')[:1]
+    protein = Recipe.objects.filter(role='PROTEIN').order_by('?')[:1]  
+    veggie = Recipe.objects.filter(role='VEGGIE').order_by('?')[:1] 
     bowl = [sauce[0], base[0], protein[0], veggie[0]]
-    
 
-    '''
-    for r in bowl:
-        for t in r.recipe_ingredient_set.all():
-            print t
-        r.ingredient_list = [i.amount + ' ' + j.name for i, j in zip(r.recipe_ingredient_set.all(), r.ingredients.all())]
-    '''
     data = {"ingredients": ingredients,
             "cuisines": cuisines,
             "bowl": bowl}
