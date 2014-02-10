5745d93e8578f9199266e6abb80b6575a0247c3e
sara <saradgore@gmail.com>
2014-02-09 19:57:13 -0600

    cleaned up JS

--- bowls/static/bowls/js/main.js
+++ bowls/static/bowls/js/main.js
@@ -1,55 +1,29 @@
 $(function() {
+  // TODO: make it DRYer.
   $('#use').on('click', function(event) {
-        event.preventDefault(); // To prevent following the link (optional)
-        $('#ingredient-list').removeClass('hidden');
-        $('#ingredient-list').addClass('show');
+        event.preventDefault(); 
         
         fields = $('#ingredient-list').find("input[name^='dontuse']");
         fields.attr('name', 'use');
         
-        if ($('#use-label').hasClass('hidden')) {
-            $('#use-label').removeClass('hidden');
-            $('#use-label').addClass('show');
-        };
-        if ($('#dontuse-label').hasClass('show')) {
-            $('#dontuse-label').removeClass('show');
-            $('#dontuse-label').addClass('hidden');
-        };
-        if ($('#cuisine-form').hasClass('hidden')) {
-            $('#cuisine-form').removeClass('hidden');
-            $('#cuisine-form').addClass('show');
-        };
-        if ($('#submitbutton').hasClass('hidden')) {
-            $('#submitbutton').removeClass('hidden');
-            $('#submitbutton').addClass('show');
-        };
+        $('#ingredient-list').toggleClass('hidden', false);
+        $('#use-label').toggleClass('hidden', false);
+        $('#dontuse-label').toggleClass('hidden', true)       
+        $('#cuisine-form').toggleClass('hidden', false);
+        $('#submitbutton').toggleClass('hidden', false);
   });
     
    $('#dontuse').on('click', function(event) {
-        event.preventDefault(); // To prevent following the link (optional)
-        $('#ingredient-list').removeClass('hidden');
-        $('#ingredient-list').addClass('show');
+        event.preventDefault(); 
         
         fields = $('#ingredient-list').find("input[name^='use']");
         fields.attr('name', 'dontuse');
         
-        if ($('#dontuse-label').hasClass('hidden')) {
-            $('#dontuse-label').removeClass('hidden');
-            $('#dontuse-label').addClass('show');
-        };
-        if ($('#use-label').hasClass('show')) {
-            $('#use-label').removeClass('show');
-            $('#use-label').addClass('hidden');
-        };
-        if ($('#cuisine-form').hasClass('hidden')) {
-            $('#cuisine-form').removeClass('hidden');
-            $('#cuisine-form').addClass('show');
-        };
-        if ($('#submitbutton').hasClass('hidden')) {
-            $('#submitbutton').removeClass('hidden');
-            $('#submitbutton').addClass('show');
-        };
+        $('#ingredient-list').toggleClass('hidden', false);
+        $('#use-label').toggleClass('hidden', true);
+        $('#dontuse-label').toggleClass('hidden', false)       
+        $('#cuisine-form').toggleClass('hidden', false);
+        $('#submitbutton').toggleClass('hidden', false);
   });
  });
-
 
