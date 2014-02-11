bba4ba70213d9417cfcb60db7196a405838f1ead
sara <saradgore@gmail.com>
2014-02-11 09:08:26 -0600

    Fixed bug with post and another with evaluating empty lists

--- bowls/views.py
+++ bowls/views.py
@@ -17,47 +17,42 @@ def main(request):
 
     if request.POST:
         #TODO: improve error handling
-        #Maybe make custom methods so these queries aren't so long
+        #Maybe make custom methods so these queries aren't so ridiculous
         data['submitted'] = True
-        no_bowl = False
         
         base = Recipe.objects.filter(role='BASE')
         protein = Recipe.objects.filter(role='PROTEIN')
         veggie = Recipe.objects.filter(role='VEGGIE')
         sauce = Recipe.objects.filter(role='SAUCE')
 
-        if 'cuisine' in request.POST and request.POST['cuisine'] != 'ANY': 
-            try: 
-                c_list = request.POST.getlist('cuisine')
-                base = base.filter(cuisines__name__in=c_list)
-                protein = protein.filter(cuisines__name__in=c_list)
-                veggie = veggie.filter(cuisines__name__in=c_list)
-                sauce = sauce.filter(cuisines__name__in=c_list)
-            except:
-                no_bowl = True
+        if 'cuisine' in request.POST and request.POST['cuisine'] != 'ANY':  
+            c_list = request.POST.getlist('cuisine')
+            base = base.filter(cuisines__name__in=c_list)
+            protein = protein.filter(cuisines__name__in=c_list)
+            veggie = veggie.filter(cuisines__name__in=c_list)
+            sauce = sauce.filter(cuisines__name__in=c_list)
         
-        if not no_bowl and 'dontuse' in request.POST:
-            try:
-                i_list = request.POST.getlist('dontuse')
-                base = base.exclude(ingredients__name__in=i_list)
-                protein = protein.exclude(ingredients__name__in=i_list)
-                veggie = veggie.exclude(ingredients__name__in=i_list)
-                sauce = sauce.exclude(ingredients__name__in=i_list)
-            except:
-                no_bowl = True
+        if 'dontuse' in request.POST:
+            i_list = request.POST.getlist('dontuse')
+            base = base.exclude(ingredients__name__in=i_list)
+            protein = protein.exclude(ingredients__name__in=i_list)
+            veggie = veggie.exclude(ingredients__name__in=i_list)
+            sauce = sauce.exclude(ingredients__name__in=i_list)
 
-        if not no_bowl and 'use' in request.POST:
+        if 'use' in request.POST:
             i_list = request.POST.getlist('use')
             base = base.filter(ingredients__name__in=i_list) or base
             protein = protein.filter(ingredients__name__in=i_list) or protein
             veggie = veggie.filter(ingredients__name__in=i_list) or veggie
             sauce = sauce.filter(ingredients__name__in=i_list) or sauce
         
-        if not no_bowl:
-            base = base.order_by('?')
-            protein = protein.order_by('?')
-            veggie = veggie.order_by('?')
-            sauce = sauce.order_by('?')
+        base = base.order_by('?')
+        protein = protein.order_by('?')
+        veggie = veggie.order_by('?')
+        sauce = sauce.order_by('?')
+        try:
             data['bowl'] = [base[0], protein[0], veggie[0], sauce[0]]
-            
+        except:
+            pass   
+                        
     return render(request, 'base.html', data)
