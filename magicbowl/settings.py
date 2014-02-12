30ab2bfa247ffd5452a582a5b7362b6a8bc73cb1
sara <saradgore@gmail.com>
2014-02-11 19:42:30 -0600

    blah

--- magicbowl/settings.py
+++ magicbowl/settings.py
@@ -102,7 +102,7 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_PATH, 'static'),
+    os.path.join(PROJECT_PATH, 'staticfiles'),
 )
 
 # SECURITY WARNING: keep the secret key used in production secret!
