75d6b6e39285f0991307ba915525e8af988f2c92
sara <saradgore@gmail.com>
2014-02-11 18:37:19 -0600

    more fuckign testing

--- magicbowl/settings.py
+++ magicbowl/settings.py
@@ -99,11 +99,11 @@ ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
 import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'magicbowl/staticfiles'
-STATIC_URL = 'static/'
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(PROJECT_PATH, 'staticfiles'),
 )
 
