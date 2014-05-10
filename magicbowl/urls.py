c325490002f71013cb8fc1d1f603a51a90098c5c
sara <saradgore@gmail.com>
2014-05-10 16:09:14 -0500

    changed index to generic

--- magicbowl/urls.py
+++ magicbowl/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.generic import TemplateView
 
 from bowls import views
 
@@ -9,10 +10,9 @@ admin.autodiscover()
 '''Until I get a real landing page for this site.'''
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
     url(r'^magicbowl/admin/', include(admin.site.urls)),
     url(r'^magicbowl/$', include('bowls.urls')),
-    
 )
 
 urlpatterns += staticfiles_urlpatterns()
